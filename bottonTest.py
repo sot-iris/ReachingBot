a712b198785b050418054721fabfa246b6694626
sot-iris <skakanos@hotmail.co.uk>
2020-09-08 12:42:59 +0100

    Create bottonTest.py

---
+++ bottonTest.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def isHome():
+    if GPIO.input(27) == 1:
+        return True
+    else:
+        return False
+
+while True:
+    print(isHome)
