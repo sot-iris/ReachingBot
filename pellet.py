327c82f5e42374d06ed89e12b456848a91c31032
sot-iris <skakanos@hotmail.co.uk>
2020-10-02 18:49:53 +0100

    Update pellet.py

--- pellet.py
+++ pellet.py
@@ -85,7 +85,7 @@ def blobStream():
             if pel:
                 blobs.append(pel)
                 cv2.circle(image, (int(pel.x), int(pel.y)), int(pel.size), (0, 0, 255), thickness=2, shift=0)
-            cv2.imshow("live frame", image[140:240,90:160])
+            cv2.imshow("live frame", image)#[140:240,90:160])
             cv2.waitKey(1) & 0xFF
 
 def isPellet():
