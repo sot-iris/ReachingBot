99ea41c219338db8219784e1bb56840533fadcc3
sot-iris <skakanos@hotmail.co.uk>
2020-10-14 15:59:58 +0100

    Update pellet.py

--- pellet.py
+++ pellet.py
@@ -154,3 +154,38 @@ start_stream.start()
 
 blob_stream = threading.Thread(target=blobStream)
 blob_stream.start()
+
+if __name__ == '__main__':
+    maxTrials = raw_input("Please enter the number of trials: ")
+
+    trial = 0
+    error = 0
+
+    while True:
+        try:
+            if trial < int(maxTrials):
+                if error < 5:
+                    if getPellet(): #gets a pellet from the dispenser (which turns for 3 seconds), returns true if pellet dispense is successful
+                        pLog("Trial {}".format(trial))
+                        activateTrial()
+                        trial += 1
+                        error = 0
+                    else:
+                        pLog("No pellet detected.")
+                        error += 1
+                else:
+                    pLog("Too many failed pellet retrievals")
+                    break
+            else:
+                pLog("Trials ended")
+                break
+
+        except KeyboardInterrupt:
+            stopCamera()
+            cv2.destroyAllWindows()
+            pLog("program terminated")
+            break
+
+    cv2.destroyAllWindows()
+    stopCamera()
+    print("All done here")
